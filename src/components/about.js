bfb63d14a5b22ddea49b13265e2cac165285ce4b
Leonardo Stoppani <58340900+lilf4p@users.noreply.github.com>
2024-09-02 20:54:41 +0200

    minor

--- src/components/about.js
+++ src/components/about.js
@@ -7,31 +7,31 @@ import Social from "./social";
 function isMobile() {
     const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
     return regex.test(navigator.userAgent);
-  }
+}
 
 function About() {
     return (
         <Container id="about" className="About" >
             <div className="Title">
                 {isMobile() ?
-                <p>
-                <p>Hi there,</p>
-                <p>I'm Leonardo 👋</p>
-                </p>
-                :
-                <p>Hi there, I'm Leonardo 👋</p>
+                    <p>
+                        <p>Hi there,</p>
+                        <p>I'm Leonardo 👋</p>
+                    </p>
+                    :
+                    <p>Hi there, I'm Leonardo 👋</p>
                 }
                 <Social />
                 <div className="Subtitle">
                     Follow my journey through AI and ML
-                    <p className="Description">
+                    <div className="Description">
                         Hey 👋, I’m Leonardo Stoppani, a 24-year-old student of
                         AIML with a Bachelor Degree in Computer Science from
                         Unipi. Currently i am enrolled in Master of Artificial
                         Intelligence at Unipi and I have attended one semester
                         at University of Copenhagen! Below, you can find some of
                         my projects, to see more visit my github profile!
-                    </p>
+                    </div>
                 </div>
             </div>
         </Container>
